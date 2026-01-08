4659c96eab6657ef7adad98b42ec23cb4e7c32e2
valentinomarchettti <valentinomarchettti@gmail.com>
2026-01-08 12:31:52 -0300

    postgres configuration

---
+++ src/main/java/com/platzi/play/entity/MovieEntity.java
@@ -0,0 +1,89 @@
+package com.platzi.play.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="movies")
+public class MovieEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150, unique = true)
+    private String titulo;
+
+    @Column(nullable = false, precision = 3)
+    private Integer duracion;
+
+    @Column(nullable = false, length = 40)
+    private String genero;
+
+    @Column(name = "fecha_estreno")
+    private LocalDate fechaEstreno;
+
+    @Column(precision = 3, scale = 2)
+    private BigDecimal clasificacion;
+
+    @Column(nullable = false, length = 1)
+    private String estado;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Integer duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public LocalDate getFechaEstreno() {
+        return fechaEstreno;
+    }
+
+    public void setFechaEstreno(LocalDate fechaEstreno) {
+        this.fechaEstreno = fechaEstreno;
+    }
+
+    public BigDecimal getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(BigDecimal clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
